876d546fdc7a887c696244a007c51c1bf303cf27
Joao Pedro <joao.machado0601@gmail.com>
2021-04-25 18:36:27 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,28 @@
+import '../styles/global.scss';
+
+import { Player } from '../components/Player'
+import { Header } from "../components/Header";
+
+import { PlayerContextProvider } from '../contexts/PlayerContext';
+import { useState } from 'react';
+
+
+import styles from '../styles/app.module.scss';
+
+
+function MyApp({ Component, pageProps }) {
+  
+  return (
+    <PlayerContextProvider>
+      <div className={styles.wrapper}>
+        <main>
+        <Header />
+        <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
+  )
+}
+
+export default MyApp
